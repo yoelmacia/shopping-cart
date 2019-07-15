c38a1274513d7ebca8d49711f0a33efb0834a1fe
YoelMacia <yoelmaciadelgado@gmail.com>
2019-07-15 16:44:17 +0200

    Add products mookup

--- src/App.js
+++ src/App.js
@@ -11,6 +11,18 @@ function App() {
           </ul>
         </div>
       </nav>
+      <div>
+        <span>Image: Image 1</span>
+        <span>Name: Product 1</span>
+        <span>Description: Blablabla 1</span>
+        <span>Icon Add Cart 1 </span>
+      </div>
+      <div>
+        <span>Image: Image 2</span>
+        <span>Name: Product 2</span>
+        <span>Description: Blablabla 2</span>
+        <span>Icon Add Cart 2 </span>
+      </div>
     </div>
   );
 }
