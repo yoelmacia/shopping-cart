52fb3c7c2e20af6add813692281073f9e577b80f
YoelMacia <yoelmaciadelgado@gmail.com>
2019-07-15 17:51:05 +0200

    Fetch dummy data
    Add Jsonplaceholder to receive some data when the component
    is created.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Product from "./Product";
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Product />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  receiveData() {
+    const BASEURL = "https://jsonplaceholder.typicode.com/albums/1/photos";
+    fetch(BASEURL)
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+
+  componentWillMount() {
+    this.receiveData();
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Product />
+      </div>
+    );
+  }
 }
 
 export default App;
