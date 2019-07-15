3524f9926c8ae722258e3bc69732c2d133d592e1
YoelMacia <yoelmaciadelgado@gmail.com>
2019-07-15 17:02:27 +0200

    Create product component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Product from "./Product";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div>
-        <span>Image: Image 1</span>
-        <span>Name: Product 1</span>
-        <span>Description: Blablabla 1</span>
-        <span>Icon Add Cart 1 </span>
-      </div>
-      <div>
-        <span>Image: Image 2</span>
-        <span>Name: Product 2</span>
-        <span>Description: Blablabla 2</span>
-        <span>Icon Add Cart 2 </span>
-      </div>
+      <Product />
     </div>
   );
 }
---
+++ src/Product.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+  render() {
+    return (
+      <div>
+        <span>Image: Image 1</span>
+        <span>Name: Product 1</span>
+        <span>Description: Blablabla 1</span>
+        <span>Icon Add Cart 1 </span>
+      </div>
+    );
+  }
+}
+
+export default Product;
