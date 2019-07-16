96be0351b512f0fec1696c7d50e5bc2b86e5c3a1
YoelMacia <yoelmaciadelgado@gmail.com>
2019-07-16 23:43:50 +0200

    Updates

---
+++ precache-manifest.b052777902eefe1d2c01664358f5b83d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8015294975d38ccc3db0cee0c703cdc6",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "ef5e5032e2204301e917",
+    "url": "/shopping-cart/static/css/main.1ef60771.chunk.css"
+  },
+  {
+    "revision": "84f90dade2b2a6f48cfa",
+    "url": "/shopping-cart/static/js/2.fe49647a.chunk.js"
+  },
+  {
+    "revision": "ef5e5032e2204301e917",
+    "url": "/shopping-cart/static/js/main.f430995a.chunk.js"
+  },
+  {
+    "revision": "3675f54918f9d621420d",
+    "url": "/shopping-cart/static/js/runtime~main.60a85d2f.js"
+  }
+]);
